50a3a300e1249401c975ad3c9854b3088142501f
mdesabd <esa.ce2020@gmail.com>
2023-01-23 01:00:51 +0600

    react icons setup , button group - google and github taking

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaWhatsapp, FaTwitch } from "react-icons/fa";
+
+
+
 
 const RightSideNav = () => {
     return (
         <div>
-           <h3>Right Nav</h3>
+            <ButtonGroup vertical>
+                <Button className='mb-2' variant="outline-primary"> <FaGoogle></FaGoogle> Login with Google</Button>
+                <Button variant="outline-dark"> <FaGithub></FaGithub> Login with Github</Button>
+            </ButtonGroup>
         </div>
     );
 };
