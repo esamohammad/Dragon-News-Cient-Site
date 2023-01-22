d610d5c3dadc4826bf2c34cedbf1fe8e6ed27a41
mdesabd <esa.ce2020@gmail.com>
2023-01-23 01:03:42 +0600

    list button add

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ListGroup } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaWhatsapp, FaTwitch } from "react-icons/fa";
@@ -13,6 +14,16 @@ const RightSideNav = () => {
                 <Button className='mb-2' variant="outline-primary"> <FaGoogle></FaGoogle> Login with Google</Button>
                 <Button variant="outline-dark"> <FaGithub></FaGithub> Login with Github</Button>
             </ButtonGroup>
+            <div className='mt-4'>
+                <h5>Find us on</h5>
+                <ListGroup>
+                    <ListGroup.Item className='mb-2'><FaFacebook /> Facebook</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaWhatsapp/> WhatsApp</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitter/> Twitter</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitch/> Twitch</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'>Vestibulum at eros</ListGroup.Item>
+                </ListGroup>
+            </div>
         </div>
     );
 };
