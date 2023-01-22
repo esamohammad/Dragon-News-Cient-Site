89c070bea745eff610179cf678041276edebc65f
mdesabd <esa.ce2020@gmail.com>
2023-01-23 00:32:21 +0600

    data load done on ui and map is so charming

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 
 //data loaded for the help of corse , fetch done and chech  <h4>All Category: {categories.length}</h4>
@@ -6,18 +7,32 @@ import React, { useEffect, useState } from 'react';
 const LeftSideNav = () => {
     const [categories, setCategories] = useState([]);
 
-    useEffect( () =>{
+    useEffect(() => {
         fetch('http://localhost:5000/news-categories')
-        .then( res => res.json())
-        .then(data => setCategories(data));
+            .then(res => res.json())
+            .then(data => setCategories(data));
     }, [])
 
 
     return (
         <div>
-          <h4>All Category: {categories.length}</h4>
+            <h4>All Category: {categories.length}</h4>
+            <div>
+                {
+                    categories.map(category => 
+                    <p key={category.id}>
+                        <Link to={`/category/${category.id}`}>{category.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
 
 export default LeftSideNav;
+// 💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥
+
+//unique key is attribute of p ...<p Key={}>   *****   </p>
+// map is here very charming , so notice it and try to understand 
+
+// 💖💖💥🗼🗼🗽🌄🌄🚚🚚🚚🏡🔰🔱🌈🌈🌟🌟💥💥💥💥💥💥💥💥💥
