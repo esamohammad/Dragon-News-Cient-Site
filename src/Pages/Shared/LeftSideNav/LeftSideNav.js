9a5c378c1d3fba30c85d17056e73233137e98892
mdesabd <esa.ce2020@gmail.com>
2023-01-23 00:14:30 +0600

    data loaded and cors policy done

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+
+//data loaded for the help of corse , fetch done and chech  <h4>All Category: {categories.length}</h4>
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect( () =>{
+        fetch('http://localhost:5000/news-categories')
+        .then( res => res.json())
+        .then(data => setCategories(data));
+    }, [])
+
+
     return (
         <div>
-           <h3>Left Nav</h3>
+          <h4>All Category: {categories.length}</h4>
         </div>
     );
 };
