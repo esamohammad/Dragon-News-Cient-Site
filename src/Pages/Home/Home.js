8e97cda2d5f3f7f4847f925cc0f069c5d97fa84e
mdesabd <esa.ce2020@gmail.com>
2023-01-23 07:08:14 +0600

    News summary card and 26 title

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import NewsSummaryCard from '../Shared/NewsSummaryCard/NewSummaryCard';
 
 const Home = () => {
    const allNews = useLoaderData();
    return (
       <div className='App'>
          <h2>Dragon News Home: {allNews.length}</h2>
+         {
+                allNews.map(news => <NewsSummaryCard
+                    key={news._id}
+                    news={news}
+                ></NewsSummaryCard>)
+            }
       </div>
    );
 };
---
+++ src/Pages/Shared/NewsSummaryCard/NewSummaryCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewSummaryCard = ({news}) => {
+   return (
+      <div>
+         {news.title}
+      </div>
+   );
+};
+
+export default NewSummaryCard;
