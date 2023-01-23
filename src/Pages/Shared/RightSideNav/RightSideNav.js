8bf95793ad6fb7f7b3157ee5347b3bd6ca202937
mdesabd <esa.ce2020@gmail.com>
2023-01-24 01:50:31 +0600

    button click and pop up open

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import { GoogleAuthProvider } from 'firebase/auth';
+import React, { useContext } from 'react';
 import { ListGroup } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaWhatsapp, FaTwitch } from "react-icons/fa";
+import { AuthContext } from '../../../countext/AuthProvider/AuthProvider';
 import BrandCarousel from '../BrandCarousel/BrandCarousel';
 
 
 
 const RightSideNav = () => {
+
+    const { providerLogin } = useContext(AuthContext);
+    const googleProvider = new GoogleAuthProvider()
+
+    const handleGoogleSingIn = () => {
+        providerLogin(googleProvider)
+            .then((result) => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch((error) => console.error(error))
+
+    }
+
     return (
         <div>
             <ButtonGroup vertical>
-                <Button className='mb-2' variant="outline-primary"> <FaGoogle></FaGoogle> Login with Google</Button>
+                <Button onClick={handleGoogleSingIn} className='mb-2' variant="outline-primary"> <FaGoogle></FaGoogle> Login with Google</Button>
                 <Button variant="outline-dark"> <FaGithub></FaGithub> Login with Github</Button>
             </ButtonGroup>
             <div className='mt-4'>
                 <h5>Find us on</h5>
                 <ListGroup>
                     <ListGroup.Item className='mb-2'><FaFacebook /> Facebook</ListGroup.Item>
-                    <ListGroup.Item className='mb-2'><FaWhatsapp/> WhatsApp</ListGroup.Item>
-                    <ListGroup.Item className='mb-2'><FaTwitter/> Twitter</ListGroup.Item>
-                    <ListGroup.Item className='mb-2'><FaTwitch/> Twitch</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaWhatsapp /> WhatsApp</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitter /> Twitter</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitch /> Twitch</ListGroup.Item>
                     <ListGroup.Item className='mb-2'>Vestibulum at eros</ListGroup.Item>
                 </ListGroup>
             </div>
--- src/countext/AuthProvider/AuthProvider.js
+++ src/countext/AuthProvider/AuthProvider.js
@@ -1,10 +1,19 @@
 import React, { createContext } from 'react';
+import { getAuth, signInWithPopup } from 'firebase/auth'
+import app from '../../Firebase/Firebase.config';
+
+
 export const AuthContext = createContext();
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
    const user = { displayName: "Batas Ali" }
 
-   const authInfo = { user };
+   const providerLogin = (provider) => {
+      return signInWithPopup(auth, provider);
+  }
+
+   const authInfo = { user,providerLogin };
 
    return (
       <AuthContext.Provider value={authInfo}>
