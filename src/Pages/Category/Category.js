f7b422ba33c8a5c15d8872ddf237f68e5e45b929
mdesabd <esa.ce2020@gmail.com>
2023-01-23 21:14:30 +0600

    categori wise news showed

--- src/Pages/Category/Category.js
+++ src/Pages/Category/Category.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-
+import NewsSummaryCard from '../Shared/NewsSummaryCard/NewSummaryCard';
 const Category = () => {
+    
     const categoryNews = useLoaderData();
     return (
         <div>
             <h2>This is Category has news: {categoryNews.length}</h2>
-            
+            {
+                categoryNews.map(news =><NewsSummaryCard
+                    key={news._id}
+                    news={news}
+                ></NewsSummaryCard>)
+            }
         </div>
     );
 };
